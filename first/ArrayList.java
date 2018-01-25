4fbab5d5f6a1b1154c68225ac17082b6218d7e25
nchristiny <icarus747@gmail.com>
2018-01-25 17:56:13 -0600

    Initial commit

---
+++ first/ArrayList.java
@@ -0,0 +1,129 @@
+/*
+ * CS 304 Assignment 1: Customizing Java class and interface
+ */
+ 
+/**
+ *
+ * @author Nicholas Christiny
+ */
+ 
+public class ArrayList implements List {
+
+   private int[] data;
+   private int next;
+   
+   public ArrayList(int initialSize)
+   {
+      data = new int[initialSize];
+      next = 0;
+   }
+   
+   public void add(int val)
+   {
+      data[next] = val;
+      next = next + 1; 
+   }
+   
+   public int remove(int idx)
+   // return removed value and shift all higher indices, or return -1 
+   {
+      if (idx >= next || idx < 0) 
+      {
+         return -1;
+      }
+      int removedData = data[idx];
+      for(int i = idx;i < next - 1;i++)
+      {
+         data[i] = data[i + 1];
+      }
+      next = next - 1;
+      data[next] = 0;
+      return removedData;
+   }
+   
+   public int get(int idx)
+   {  
+      int value;
+      value = this.data[idx];
+      return value;
+   }
+   
+   public void set(int idx, int val)
+   {
+      data[idx] = val;   
+   }
+   
+   public void sort()
+   // Don't use Array.sort()
+   // We could even make this more efficient by looping while checking boolean flag
+   {  
+      for (int j = 0;j < next;j++) {
+         for (int i = 1; i < next - j; i++)
+         {
+            if (data[i] < data[i - 1])
+            {
+               int temp = data[i];
+               data[i] = data[i-1];
+               data[i-1] = temp;
+            }
+         }
+      }
+   }
+   
+   public int size()
+   {  
+      int count = this.next;
+      return count;
+   }
+   
+   public void clear()
+   {
+      next = 0;
+   }
+   
+   public int indexOf(int val)
+   // Used in contains() method
+   {
+      for(int i = 0;i < next;i++){
+         if (data[i] == val)
+            return i;
+         }
+      return -1; 
+   }
+   
+   public boolean isEmpty()
+   {
+      return (this.size() == 0);
+   }
+   
+   public boolean isFull()
+   {
+      // never full
+      return false;
+   }
+   
+   public boolean contains(int val)
+   {
+      return (indexOf(val) != -1);
+   }
+   
+   public void reverse()
+   {
+   
+   }
+    
+   public String toString() {   
+      // may implement iteratable in future
+      String output = "";  
+      for(int i = 0; i < this.size(); i++) 
+      {
+         if (i == this.size() - 1)
+         {
+            output = output + this.get(i);
+         } 
+         else 
+            output = output + this.get(i) + ", ";
+      }
+      return output;
+    }
+}
---
+++ first/ArrayListTester.java
@@ -0,0 +1,77 @@
+public class ArrayListTester {
+
+    public static void main(String[] args)
+   {
+      int maxVal = 100;
+      int numVals = (int)(Math.random() * 16) + 8;
+      List myList = new ArrayList(numVals); 
+      System.out.println("Adding: ");
+      
+      for(int i = 0; i < numVals; i++) 
+      {
+         int val = (int)(Math.random()*maxVal) + 1;
+         myList.add(val);
+         System.out.print(val + " ");
+      }
+      System.out.println();
+      System.out.println("myList Size is " + myList.size());
+      System.out.println(myList);
+      System.out.println();
+      
+      for(int i=0;i<myList.size()/4;i++) 
+      {
+         int idx = (int)(Math.random() * myList.size());
+         int val = myList.get(idx);
+         System.out.println(val + " is at index: " + idx + ".  removing...");         
+         myList.remove(idx);
+      }      
+      System.out.println();
+      System.out.println("Resulting list after removing: ");
+      System.out.println(myList);
+      System.out.println("myList size is " + myList.size());
+      System.out.println();
+
+      for(int i=0; i<myList.size()/2; i++) 
+      {
+         int idx = (int)(Math.random() * myList.size());
+         int val = myList.get(idx);
+         int newVal = (int)(Math.random() * maxVal) + 1;
+         System.out.println("Changing the " + val + " at index " + idx + " to " + newVal + ".");
+         myList.set(idx, newVal);
+      }      
+      System.out.println("Resulting list after changing: ");
+      System.out.println( myList );
+      System.out.println();
+      for(int i = 0;i < myList.size() * 2;i++)
+      {
+         int val = (int)(Math.random() * maxVal)+1;
+         if(myList.contains(val)) 
+         {
+            System.out.println(val + " is in the list!");
+         }
+         else
+         {
+            System.out.println(val + " is not in the list!");
+         }
+      }
+      System.out.println();
+      System.out.println("Sorting the list:");  
+      myList.sort();
+      System.out.println(myList);
+      System.out.println();
+            
+//       // Verifies newly instantiated ArrayList is empty
+//       List emptyList = new ArrayList(10); 
+//       System.out.print("Is newly created list empty? ");
+//       System.out.println(emptyList.isEmpty());
+      
+      System.out.println("Removing all the elements from myList!");
+       while( myList.isEmpty() == false )
+       {
+          myList.remove(0);
+       }      
+      System.out.println("Size of mySize after emptying = " + myList.size());
+      System.out.println("myList: " + myList);
+      System.out.println("Goodbye!");
+   }
+}
