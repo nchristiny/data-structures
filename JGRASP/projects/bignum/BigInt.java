e803d0bb3726168db93719e32791812e1e248533
nchristiny <icarus747@gmail.com>
2018-03-13 21:27:12 -0500

    Add constructor from BigInt

--- JGRASP/projects/bignum/BigInt.java
+++ JGRASP/projects/bignum/BigInt.java
@@ -29,7 +29,7 @@ public class BigInt implements Comparable<BigInt> {
       
       for (int i = 0; i <= newNum.length() - 1; i++) {
          /* obtain the int value of each character in string
-          and add it to data */      
+          and add it to data linked list */      
          if (newNum.charAt(i) == '0'){
             // Skip leading zeros
             continue; 
@@ -42,7 +42,8 @@ public class BigInt implements Comparable<BigInt> {
    public BigInt(BigInt num) {
       /* create duplicate of num, 
       data reference should be new */
-
+      this.data = num.data;
+      this.isNeg = num.isNeg;
    }
    
    public BigInt abs(BigInt a) {
@@ -120,8 +121,8 @@ public class BigInt implements Comparable<BigInt> {
       BigInt myNum = new BigInt("12345");
       BigInt myNum2 = new BigInt("-12345");
       BigInt myNum3 = new BigInt("+0000001243523452345234522345");
-      BigInt myNum01 = new BigInt("123");
-      BigInt myNum02 = new BigInt("-1232");
+      BigInt myNum01 = new BigInt("-123");
+      BigInt myNum02 = new BigInt("-0012341293487192857623948756293487562938457236046981406");
       System.out.println(myNum);
       System.out.println(myNum2);
       System.out.println(myNum3);
@@ -130,6 +131,9 @@ public class BigInt implements Comparable<BigInt> {
       System.out.println(myNum3.toFormattedString());
       System.out.println(myNum01.toFormattedString());
       System.out.println(myNum02.toFormattedString());
+      
+      BigInt duplicatedBigInt = new BigInt(myNum02);
+      System.out.println(duplicatedBigInt.toFormattedString());
 
    }
 }
