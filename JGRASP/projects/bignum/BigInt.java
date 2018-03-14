32b58ce20b74e2da34bb873a099b222d6b927be3
nchristiny <icarus747@gmail.com>
2018-03-13 21:17:10 -0500

    Add toFormattedString method

--- JGRASP/projects/bignum/BigInt.java
+++ JGRASP/projects/bignum/BigInt.java
@@ -29,7 +29,11 @@ public class BigInt implements Comparable<BigInt> {
       
       for (int i = 0; i <= newNum.length() - 1; i++) {
          /* obtain the int value of each character in string
-          and add it to data */
+          and add it to data */      
+         if (newNum.charAt(i) == '0'){
+            // Skip leading zeros
+            continue; 
+         } 
          int digit = Character.getNumericValue(newNum.charAt(i));
          data.add(digit);
       }    
@@ -66,14 +70,46 @@ public class BigInt implements Comparable<BigInt> {
    }   
    
    public String toFormattedString() {
-      String r = this.toString();
-      /* start at end of r, work backwards and
+       /* start at end of r, work backwards and
          insert commas every three characters */
-      return r;
+       String r = this.toString();
+      
+       // check to see if format is necessary
+       if (this.isNeg == true && r.length() - 1 < 4){
+           return r;
+       } else if (this.isNeg == false && r.length() < 4) {
+           return r;
+       }
+      
+       String canonical = "";
+       if (this.isNeg == true) {
+           canonical = r.substring(1);
+       } else {
+           canonical = r;
+       }
+      
+       String prettyPrint = "";
+       int count = 0;
+       for (int i = canonical.length() - 1; i >= 0; i--) {
+           prettyPrint = canonical.charAt(i) + prettyPrint;
+           count++;
+           if ((count % 3 == 0) && (i > 0)) {
+               prettyPrint = "," + prettyPrint;
+           }
+       }
+       
+       if (this.isNeg == true) {
+           return "-" + prettyPrint; 
+       } else {
+           return prettyPrint;
+       }
    }
    
    public String toString() {
       String r = "";
+      if (this.isNeg == true) {
+         r = "-";
+      }
       for (int i = 0; i <= data.size() - 1; i++){
          r += data.get(i);
       }
@@ -84,8 +120,16 @@ public class BigInt implements Comparable<BigInt> {
       BigInt myNum = new BigInt("12345");
       BigInt myNum2 = new BigInt("-12345");
       BigInt myNum3 = new BigInt("+0000001243523452345234522345");
+      BigInt myNum01 = new BigInt("123");
+      BigInt myNum02 = new BigInt("-1232");
       System.out.println(myNum);
       System.out.println(myNum2);
       System.out.println(myNum3);
+      System.out.println(myNum.toFormattedString());
+      System.out.println(myNum2.toFormattedString());
+      System.out.println(myNum3.toFormattedString());
+      System.out.println(myNum01.toFormattedString());
+      System.out.println(myNum02.toFormattedString());
+
    }
 }
